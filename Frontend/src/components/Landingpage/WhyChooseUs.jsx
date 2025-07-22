1b64677529e7b5bf8fb6e4ee7be07317225b329a
Kshitij Gupta <kshitijgupta2162@gmail.com>
2025-07-22 13:10:39 +0530

    updated

--- Frontend/src/components/Landingpage/WhyChooseUs.jsx
+++ Frontend/src/components/Landingpage/WhyChooseUs.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { CheckCircle } from "lucide-react"; // optional icon library
+import {  CheckCircle } from "lucide-react"; // optional icon library
 import headImage from '../../assets/head.png'; // Adjust the path as needed
 import { useNavigate } from "react-router-dom";
+import RightArrow from '../../assets/right-arrow.png'
 
 
 const WhyChooseUs = () => {
 
-const navigate = useNavigate();
-      const openForm = () => {
-    navigate('/HealthForm'); // Redirects to the "contact-us" page
-  };
+    const navigate = useNavigate();
+    const openForm = () => {
+        navigate('/HealthForm'); // Redirects to the "contact-us" page
+    };
     return (
         <section className="bg-white py-6 md:py-12 px-4 md:px-16 lg:px-24" id="why-choose-us">
             <div className="max-w-8xl mx-auto">
@@ -58,15 +59,26 @@ const navigate = useNavigate();
                 </div>
 
                 <div className="mt-10 text-center text-gray-600">
-                     <h1 className="text-black text-lg font-bold">
-                        Note from Dr Parul Agrawal : </h1>
+                    <h1 className="text-black text-lg font-bold">
+                        Note from Dr Parul Agrawal</h1>
                     <p className="text-red-500 text-lg md:text-xl pt-1">
-                        If you're struggling with issues like Obesity, Diabetes, Hyper Tension, PCOS, thyroid imbalance, sleep disturbances, hair fall, diabetes, or constant fatigue — it's time to stop masking symptoms and address the root cause.
+                        If you're struggling with issues like Obesity, Diabetes, Hypertension, PCOS, thyroid imbalance, sleep disturbances, hair fall, diabetes, or constant fatigue — it's time to stop masking symptoms and address the root cause.
                     </p>
+                    <p className="mt-6 text-xl font-semibold text-gray-800 flex justify-center items-start">
+                        Click below
+
+                        {/* Arrow Icon with style */}
+                        <img src={RightArrow}
+                            className="mt-4 text-green-600 w-10 h-10 animate-bounce transition-all duration-500 hover:scale-125 drop-shadow-[0_0_4px_rgba(34,197,94,0.8)]"
+                        />
+                        
+                    </p>
+
+
                     <button
-                    onClick={openForm}
-                    class="mt-6 py-3 px-6 bg-gradient-to-r from-black via-green-500 to-green-800 text-white font-extrabold text-md rounded-2xl shadow-xl border-2 border-white animate-bounce hover:scale-110 hover:shadow-2xl transition-all duration-500 hover:ring-4 hover:ring-pink-300">
-                        ✨ Fill the Form Now ✨
+                        onClick={openForm}
+                        class="mt-6 py-3 px-6 bg-gradient-to-r from-black via-green-500 to-green-800 text-white font-extrabold text-md rounded-2xl shadow-xl border-2 border-white animate-bounce hover:scale-110 hover:shadow-2xl transition-all duration-500 hover:ring-4 hover:ring-pink-300">
+                        ✨ Evaluate Your Health ✨
                     </button>
 
 
