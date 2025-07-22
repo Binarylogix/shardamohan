b294fc70548c5cac54f323238c773d505f9862a4
Kshitij Gupta <kshitijgupta2162@gmail.com>
2025-07-22 17:06:14 +0530

    updated

--- Frontend/src/models/ContactUs.jsx
+++ Frontend/src/models/ContactUs.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import Navbar from "../components/Main/Navbar";
 import Footer from "../components/Main/Footer";
@@ -195,6 +195,9 @@ Swal.fire({
   }
 };
 
+useEffect(() => {
+  window.scrollTo(0, 0);
+}, []);
 
   return (
     <>
